102bdb7ce01327f8bb3628d7e2aaaa0f0ef39f79
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2023-04-17 02:02:20 +0200

    add tailwind initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const  clampBuilder = ( minWidthPx, maxWidthPx, minFontSize, maxFontSize ) => {
+  
+  const minWidth = minWidthPx / 10 // 1 rem = 10px ;
+  const maxWidth = maxWidthPx / 10 // 1 rem = 10px ;
+
+  const slope = ( maxFontSize - minFontSize ) / ( maxWidth - minWidth );
+  const yAxisIntersection = -minWidth * slope + minFontSize
+
+  return `clamp( ${ minFontSize }rem, ${ yAxisIntersection }rem + ${ slope * 100 }vw, ${ maxFontSize }rem )`;
+}
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -6,13 +18,55 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily:{
+      'Barlow': ['Barlow Condensed', 'sans-serif'],
+      'Bellefair' : ['Bellefair', 'serif']
+    },
+    screens:{
+      'mobile' : "576px",
+      'tablet' : "768px",
+      'pre-laptop': '992px',
+      'laptop' : "1200px",
+      'desktop' : "1440px"
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize : {
+        'h1' : clampBuilder(375, 768, 8, 15),
+        'h2' : clampBuilder(375, 768, 5.6, 10),
+        'h3' : clampBuilder(375, 768, 2.4, 5.6),
+        'h4' : clampBuilder(375, 768, 1.6, 3.2),
+        'h5' : clampBuilder(375, 768, 1.6, 2.8),
+        'sub-h1' : '2.8rem',
+        'sub-h2' : '1.4rem',
+        'nav-link' : clampBuilder(375, 768, 1.4, 1.6),
+        'body-text' : clampBuilder(375, 768, 1.5, 1.8),
       },
+      colors: {
+        "lighting": "#FFFFFF",
+        "light-blue": "#D0D6F9",
+        "darking": "#0B0D17",
+      },
+      backgroundColor: {
+        "lighting": "#FFFFFF",
+        "light-blue": "#D0D6F9",
+        "darking": "#0B0D17",
+      },
+      backgroundImage: {
+        'home-mobile': "url('./assets/home/background-home-mobile.jpg')",
+        'home-tablet': "url('./assets/home/background-home-tablet.jpg')",
+        'home-desktop': "url('./assets/home/background-home-desktop.jpg')",
+        'destination-mobile': "url('./assets/destination/background-destination-mobile.jpg')",
+        'destination-tablet': "url('./assets/destination/background-destination-tablet.jpg')",
+        'destination-desktop': "url('./assets/destination/background-destination-desktop.jpg')",
+        'crew-mobile': "url('./assets/crew/background-crew-mobile.jpg')",
+        'crew-tablet': "url('./assets/crew/background-crew-tablet.jpg')",
+        'crew-desktop': "url('./assets/crew/background-crew-desktop.jpg')",
+        'technology-mobile': "url('./assets/technology/background-technology-mobile.jpg')",
+        'technology-tablet': "url('./assets/technology/background-technology-tablet.jpg')",
+        'technology-desktop': "url('./assets/technology/background-technology-desktop.jpg')",
+      }
     },
   },
   plugins: [],
 }
+
