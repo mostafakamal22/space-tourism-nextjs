8017773bf9c48d72d2276e658075753cec21ebb7
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2023-04-19 01:27:35 +0200

    add not found page

--- app/components/NotFound.tsx
+++ app/components/NotFound.tsx
@@ -1,29 +1,23 @@
 import Link from "next/link";
 import notFoundImg from "public/assets/shared/not-found.svg";
-import { CONTAINER, NOTFOUND } from "@/utils/tailwindClasses";
 import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div
-      className={
-        CONTAINER.container.join(" ") +
-        " bg-technology-mobile tablet:bg-technology-tablet laptop:bg-technology-desktop"
-      }
-    >
-      <div className={NOTFOUND.div.join(" ")}>
-        <figure className={NOTFOUND.figure.join(" ")}>
+    <div className="min-h-[100vh] font-Barlow  bg-no-repeat bg-cover bg-center  bg-technology-mobile tablet:bg-technology-tablet laptop:bg-technology-desktop">
+      <div className="min-h-[100vh] w-[100%] p-[3rem] flex flex-col justify-center items-center font-Bellefair  text-center text-lighting pre-laptop:flex-row pre-laptop:gap-4">
+        <figure className="pre-laptop:basis-[40%]">
           <Image src={notFoundImg} alt="error 404" />
         </figure>
 
         <section className="max-w-[50rem] pre-laptop:basis-auto">
-          <p className={NOTFOUND.heading2.join(" ")}>ERROR 404</p>
+          <p className="text-h2 leading-none mb-[2rem]">ERROR 404</p>
 
-          <p className={NOTFOUND.heading3.join(" ")}>Page Not Found !</p>
+          <p className="text-h3 leading-none mb-[1rem]">Page Not Found !</p>
 
-          <p className={NOTFOUND.text.join(" ")}>
+          <p className="text-body-text">
             Go Home Page From
-            <Link className={NOTFOUND.link.join(" ")} href="/">
+            <Link className="ml-[.5rem] text-light-blue" href="/">
               Here
             </Link>
           </p>
