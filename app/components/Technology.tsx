bcb6539585ee78a1f8404658fa14e607462fc73f
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2023-04-22 02:40:26 +0200

    add technologies page to the layout

--- app/components/Technology.tsx
+++ app/components/Technology.tsx
@@ -1,92 +1,99 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { handleUpdate } from "../../utils/animateImgs";
-import { technologies } from "../../utils/data";
-import { CONTAINER, TECHNOLOGY } from "../../utils/tailwindClasses";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Technology, technologies } from "../../utils/data";
+import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
+import Image from "next/image";
+import { EffectFade } from "swiper";
 
 function Technology() {
   //change between portrait/landsacpe image
   const [isPortrait, setIsPortrait] = useState(window.innerWidth);
-  window.addEventListener("resize", () => {
-    setIsPortrait(window.innerWidth);
-  });
 
   //state for active crew member
-  const [activeTechnology, setActiveTechnology] = useState([
-    ...Object.values(technologies[0]),
-  ]);
+  const [activeTechnology, setActiveTechnology] = useState<Technology>(
+    technologies[0]
+  );
 
-  //update tabs active state
+  const swiperRef = useRef<SwiperRef>(null);
+
+  const carouselImages = useMemo(
+    () =>
+      technologies.map((technology) => (
+        <SwiperSlide key={technology.id}>
+          <Image
+            src={isPortrait >= 992 ? technology.image[1] : technology.image[0]}
+            width={"200"}
+            height={"200"}
+            alt={technology.name}
+          />
+        </SwiperSlide>
+      )),
+    [isPortrait]
+  );
+
   useEffect(() => {
-    const technologiesTabs =
-      document.getElementsByClassName("technologies-tabs");
-    //remove active classe from all tabs first
-    for (const technologyTab of technologiesTabs) {
-      technologyTab.classList.remove("!bg-lighting", "!text-darking");
-    }
-    //then add the active class to the active tab only
-    technologiesTabs[activeTechnology[3]].classList.add(
-      "!bg-lighting",
-      "!text-darking"
-    );
-  }, [activeTechnology]);
+    window.addEventListener("resize", () => {
+      setIsPortrait(window.innerWidth);
+    });
+
+    return () => {
+      window.addEventListener("resize", () => {
+        setIsPortrait(window.innerWidth);
+      });
+    };
+  }, [isPortrait]);
 
   return (
     <div
       className={
-        CONTAINER.container.join(" ") +
-        "bg-technology-mobile tablet:bg-technology-tablet laptop:bg-technology-desktop"
+        "min-h-[100vh] font-Barlow  bg-no-repeat bg-cover bg-center bg-technology-mobile tablet:bg-technology-tablet laptop:bg-technology-desktop"
       }
     >
-      <main className={TECHNOLOGY.mainClasses.join(" ")}>
-        <section className={TECHNOLOGY.mainSecClasses.join(" ")}>
-          <header className={TECHNOLOGY.secHeading.join(" ")}>
-            <h5 className={TECHNOLOGY.header.join(" ")}>
+      <main className="min-h-[85vh] flex justify-center items-center text-center text-lighting laptop:min-h-[100vh] laptop:text-left laptop:pt-[14rem]">
+        <section className="h-[100%] w-[100%] max-w-[130rem] flex flex-col items-center tablet:p-[3rem] tablet:px-0 pre-laptop:grid pre-laptop:grid-cols-[1.2fr_.8fr] pre-laptop:grid-rows-[.25fr_.75fr] pre-laptop:gap-4 pre-laptop:mx-[3rem] laptop:mx-[4rem]">
+          <header className="w-[100%] text-center tablet:px-[3rem] pre-laptop:col-span-1 pre-laptop:row-span-1">
+            <h5 className="text-h5 tracking-[.3rem] uppercase tablet:text-left tablet:text-[2rem] laptop:text-[2.8rem] laptop:tracking-[.3rem] laptop:mb-[3rem]">
               <span className="text-lighting/[.25]">03</span> SPACE LAUNCH 101
             </h5>
           </header>
 
-          <figure className={TECHNOLOGY.figure.join(" ")}>
-            {/* <img
-              className="tech-img transition-all duration-[450ms] ease-in-out"
-              src={
-                isPortrait >= 992
-                  ? activeTechnology[1][1]
-                  : activeTechnology[1][0]
-              }
-              alt="space Technology"
-            ></img> */}
+          <figure className="w-[100%] h-[17rem] flex justify-center my-[3rem] tablet:h-[31rem] pre-laptop:col-start-2 pre-laptop:col-end-3 pre-laptop:row-start-1 pre-laptop:row-end-3 pre-laptop:justify-self-end pre-laptop:self-end laptop:w-[51.5rem] laptop:h-[52.7rem]">
+            <Swiper
+              ref={swiperRef}
+              fadeEffect={{ crossFade: true }}
+              modules={[EffectFade]}
+              effect="fade"
+              onSlideChange={(swiper) => {
+                setActiveTechnology(technologies[swiper.activeIndex]);
+              }}
+            >
+              {carouselImages}
+            </Swiper>
           </figure>
 
-          <div className={TECHNOLOGY.infoDev.join(" ")}>
-            <ul className={TECHNOLOGY.list.join(" ")}>
-              {technologies.map((technology) => (
+          <div className="max-w-[55rem] flex flex-col text-center px-[3rem] pre-laptop:grid pre-laptop:grid-cols-[.2fr_.8fr] pre-laptop:grid-rows-[.2fr_.3fr_.5fr] pre-laptop:gap-2  pre-laptop:text-left pre-laptop:col-span-1 pre-laptop:row-start-2 pre-laptop:row-end-3 pre-laptop:max-w-[70rem]">
+            <ul className="flex justify-center mb-[2rem] pre-laptop:col-start-1 pre-laptop:col-end-2 pre-laptop:row-start-1 pre-laptop:row-end-4 pre-laptop:self-center pre-laptop:justify-self-center  pre-laptop:flex-col pre-laptop:mb-0">
+              {technologies.map((technology, index) => (
                 <li
-                  className={TECHNOLOGY.technologiesTabs.join(" ")}
-                  key={technology.indexId}
-                  onClick={() =>
-                    handleUpdate(
-                      technology.indexId,
-                      "tech-img",
-                      activeTechnology,
-                      setActiveTechnology,
-                      technologies
-                    )
-                  }
+                  className="flex justify-center items-center text-sub-h2 font-Bellefair font-bold w-[4rem] h-[4rem] rounded-full border-[.1rem] border-lighting/[.25] mx-[.5rem] transition-all tablet:mx-[.75rem] pre-laptop:mx-0 pre-laptop:my-[1.6rem]"
+                  key={technology.id}
+                  onClick={() => swiperRef?.current?.swiper?.slideTo(index)}
                 >
-                  {technology.indexId + 1}
+                  {index + 1}
                 </li>
               ))}
             </ul>
 
-            <h4 className={TECHNOLOGY.header4.join(" ")}>THE TERMINOLOGY…</h4>
+            <h4 className="text-nav-link text-light-blue tracking-[.2rem] font-Bellefair uppercase tablet:mt-[3rem] pre-laptop:col-start-2 pre-laptop:col-end-3 pre-laptop:row-start-1 pre-laptop:row-end-2">
+              THE TERMINOLOGY…
+            </h4>
 
-            <h3 className={TECHNOLOGY.header3.join(" ")}>
-              {activeTechnology[0]}
+            <h3 className="text-h3 font-Bellefair uppercase pre-laptop:col-start-2 pre-laptop:col-end-3 pre-laptop:row-start-2 pre-laptop:row-end-3">
+              {activeTechnology.name}
             </h3>
 
-            <p className={TECHNOLOGY.paragraph.join(" ")}>
-              {activeTechnology[2]}
+            <p className="max-w-[50rem] text-body-text text-light-blue  mb-[3rem] tablet:mb-[4rem] pre-laptop:col-start-2 pre-laptop:col-end-3 pre-laptop:row-start-3 pre-laptop:row-end-4">
+              {activeTechnology.description}
             </p>
           </div>
         </section>
